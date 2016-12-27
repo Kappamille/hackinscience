677e89ecb71a106be5bb383d7e1f09f5baa57d8e
Camille Duquesne <Camille_Duquesne@MacBook-de-Camille.local>
2016-12-27 22:31:48 +0100

    rev 019

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -3,7 +3,7 @@ import sys
 if len(sys.argv) != 3:
     print('usage: python3 solution.py OP1 OP2')
 else:
-    for neko in [sys.argv[1], sys.argv[2]]:
+    for neko in sys.argv[1] and sys.argv[2]:
         if neko not in "0123456789":
             print("Error, OP1 and OP2 must be positive integars")
             exit(0)
