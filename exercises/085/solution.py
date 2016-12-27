5916376379c50b09f7168ad90a8ea05219246bdd
Camille Duquesne <Camille_Duquesne@MacBook-de-Camille.local>
2016-12-27 20:03:57 +0100

    rev 085

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -2,12 +2,14 @@
 def sort_a_list(l):
     return(sorted(l, reverse=True))
 
+
 def sort_by_mark(my_class):
     from operator import itemgetter
     bla = itemgetter(0)
-    return(sorted(my_class, key = bla, reverse=True))
-	
+    return(sorted(my_class, key=bla, reverse=True))
+
+
 def sort_by_mark(my_class):
     from operator import itemgetter
     bla = itemgetter(1)
-    return(sorted(my_class, key = bla))
+    return(sorted(my_class, key=bla))
