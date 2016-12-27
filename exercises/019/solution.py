e5778a3cb208898a434cffbe408b71cc672bc896
Camille Duquesne <Camille_Duquesne@MacBook-de-Camille.local>
2016-12-27 21:26:30 +0100

    rev 019

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 import sys
-
-if len(sys.argv) != 3 :
-	print('usage: python3 solution.py OP1 OP2')
-else :
-	for neko in [sys.argv[1],sys.argv[2]]:
-		if neko not in "0123456789":
-			print("Error, OP1 and OP2 must be positive integars")
-			exit(1)
-	a = int(sys.argv[1])
-	b = int(sys.argv[2])
-	print(a + b)	
+if len(sys.argv) != 3:
+    print('usage: python3 solution.py OP1 OP2')
+else:
+    for neko in [sys.argv[1], sys.argv[2]]:
+        if neko not in "0123456789":
+            print("Error, OP1 and OP2 must be positive integars")
+            exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    print(a + b)
