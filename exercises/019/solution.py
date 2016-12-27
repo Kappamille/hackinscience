2ab358a3906be6937621007d9ae302cc9d278d38
Camille Duquesne <Camille_Duquesne@MacBook-de-Camille.local>
2016-12-27 21:31:51 +0100

    rev 019

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -6,7 +6,7 @@ else:
     for neko in [sys.argv[1], sys.argv[2]]:
         if neko not in "0123456789":
             print("Error, OP1 and OP2 must be positive integars")
-            exit(1)
+            exit(0)
     a = int(sys.argv[1])
     b = int(sys.argv[2])
     print(a + b)
