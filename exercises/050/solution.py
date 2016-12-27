5c5b66a827a8917864b47e6018394aec68843dee
Camille Duquesne <Camille_Duquesne@MacBook-de-Camille.local>
2016-12-28 00:07:23 +0100

    rev 050

--- exercises/050/solution.py
+++ exercises/050/solution.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
-
 somme = 0
-
 for i in range(1, 1000):
-	if i % 3 == 0 or i % 5 == 0:
-		somme = somme + i
-		
+    if i % 3 == 0 or i % 5 == 0:
+        somme = somme + i
 print(somme)
