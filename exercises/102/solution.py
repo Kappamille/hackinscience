3b3753385be3edd6db581554baf1ef9d41a866d0
Camille Duquesne <Camille_Duquesne@MacBook-de-Camille.local>
2017-01-03 22:39:11 +0100

    rev 102

--- exercises/102/solution.py
+++ exercises/102/solution.py
@@ -9,8 +9,8 @@ def check_my_city(city_name):
                 "zip_code": ['93170'],
                 "city": 'bagnolet'})
     elif city_name == "PARIS-":
-        return({"stations_nb": 3,
+        return {"stations_nb": 3,
                 "zip_code": ['75001', '75008', '75010'],
-                "city": 'paris'})
+                "city": 'paris'}
     else:
         return("Sorry! No station for your city has been found!")
